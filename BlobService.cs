f5fdae5a2e45e7fd451ba154be8ef68897a49b15
Notz05 <163431417+Notz05@users.noreply.github.com>
2024-08-30 15:37:41 +0200

    Add files via upload

---
+++ BlobService.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Storage.Blobs;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABCRetailWebApp.Services
+{
+    public class BlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public async Task UploadBlobAsync(string containerName, string blobName, Stream content)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+            var blobClient = containerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(content, true);
+        }
+
+        public async Task<IEnumerable<string>> ListImagesAsync(string containerName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            var blobItems = containerClient.GetBlobsAsync();
+            var imageNames = new List<string>();
+
+            await foreach (var blobItem in blobItems)
+            {
+                imageNames.Add(blobItem.Name);
+            }
+
+            return imageNames;
+        }
+    }
+}
---
+++ QueueService.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Queues;
+using System.Threading.Tasks;
+
+namespace ABCRetailWebApp.Services
+{
+    public class QueueService
+    {
+        private readonly QueueServiceClient _queueServiceClient;
+
+        public QueueService(QueueServiceClient queueServiceClient)
+        {
+            _queueServiceClient = queueServiceClient;
+        }
+
+        public async Task SendMessageAsync(string queueName, string message)
+        {
+            var queueClient = _queueServiceClient.GetQueueClient(queueName);
+            await queueClient.CreateIfNotExistsAsync();
+            await queueClient.SendMessageAsync(message);
+        }
+    }
+}
